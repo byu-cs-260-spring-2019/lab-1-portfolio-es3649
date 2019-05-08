848cee564348a21a4f47e4e30fb7c904cc454590
Eric Steadman <es3649@gmail.com>
2019-05-07 22:29:04 -0600

    ready for passoff

--- script.js
+++ script.js
@@ -2,11 +2,8 @@
 var mischeifManaged = false;
     
 function doMischief() {
-    console.log("Hello!");
     if (!mischeifManaged) {
-        // TODO start the video!
-        document.getElementById("mischiefBox").style.display = "inherit";
-        document.getElementById("trollFrame").play();
+        document.getElementById("mischiefBox").style.display = "block";
         mischeifManaged = true;
     } else {
         document.getElementById("mischiefBox").style.display = "none";
