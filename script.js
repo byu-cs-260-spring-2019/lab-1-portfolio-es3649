a7173d7221fba341ca0c1f6a3897e709f245b66b
Eric Steadman <es3649@gmail.com>
2019-05-07 22:19:40 -0600

    finalized javascripts

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ var mischeifManaged = false;
 function doMischief() {
     console.log("Hello!");
     if (!mischeifManaged) {
+        // TODO start the video!
         document.getElementById("mischiefBox").style.display = "inherit";
+        document.getElementById("trollFrame").play();
         mischeifManaged = true;
     } else {
         document.getElementById("mischiefBox").style.display = "none";
